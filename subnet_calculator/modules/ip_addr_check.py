16d826d7e173b45764d6acdc56603baefac0026e
datboyblu3 <dayboyblu3@protonmail.com>
2021-12-09 14:48:56 -0800

    tested validate_ip_input file

--- subnet_calculator/modules/ip_addr_check.py
+++ subnet_calculator/modules/ip_addr_check.py
@@ -1,4 +1,15 @@
 import sys
 
-#Verify each octet
-def ip_addr_check
+#Author: Daniel Edwards
+#Date: 12-09-2021
+#Validate each octet
+
+#Verify each octet in the IP Addresses
+def ip_addr_check(ip_list):
+
+	#Different conditions for the types of iput
+	#1) Single or multiple IP Addresses submited
+	#2) A file of IP Addresses submitted
+
+	
+
---
+++ subnet_calculator/modules/validate_ip_input.py
@@ -0,0 +1,40 @@
+import os.path
+import argparse
+
+#Author: Daniel Edwards
+#Date: 12-08-2021
+#Validate IP file existence
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', help='Specify the file and/or the file\'s path containing IP addresses')
+parser.add_argument('-i', '--ip', nargs='+', help='Specify IP address(es)')
+args = parser.parse_args()
+
+
+#Determine if file exists
+def validate_ip_file():
+
+	if args.file:
+		ip_file = args.file
+
+		#Verify if file exists
+		while True:
+			if os.path.isfile(ip_file):
+				print("Good to go")
+				break
+			else:
+				print("File does not exist, please try again")
+
+		selected_ip_file = open(ip_file,'r')
+		selected_ip_file.seek(0)
+		ip_list = selected_ip_file.readlines()
+		selected_ip_file.close()
+
+		return ip_list
+
+	elif args.ip:
+
+		return print(f"The IPs are {args.ip}")
+
+	
+validate_ip_file()
