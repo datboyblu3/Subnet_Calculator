3d460386282fabca2974b93785d1cdeea56723d5
datboyblu3 <dayboyblu3@protonmail.com>
2021-12-08 18:59:37 -0800

    validate_ip_file

--- subnet_calculator/subnet_calc.py
+++ subnet_calculator/subnet_calc.py
@@ -8,12 +8,24 @@ import argparse
 #Validate each octet
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', '-f', help='Specify a file containing IP addresses')
+parser.add_argument('-f', '--file', help='Specify the file and/or the file\'s path containing IP addresses')
 parser.add_argument('-i', '--ip', help='Specify IP address(es)')
 args = parser.parse_args()
 
 
-def validate_ip_octet(ip):
+#Determine if file exists
+def validate_ip_file():
+	ip_list = args.file
+
+	#Verify if file exists
+	while True:
+		if os.path.isfile(iplist):
+			print("Good to go")
+		else:
+			print("File does not exist, please try again")
+
+#Validate all IP octets 
+def validate_ip_octet():
 
 	'''
 	Read input from command line for a file of IPs, single IP or
