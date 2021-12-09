adb1e72cb395f889aaff5d9387781efec35af9ea
datboyblu3 <dayboyblu3@protonmail.com>
2021-12-08 17:07:16 -0800

    added validate_ip_octet function

--- subnet_calculator/subnet_calc.py
+++ subnet_calculator/subnet_calc.py
@@ -1,2 +1,16 @@
 import sys
 import random
+import os.path
+
+#Validate each octet
+def validate_ip_octet(ip):
+
+	try:
+
+		while True:
+			#Take input as either a file or single IP address
+
+	except KeyboardInterrupt:
+		print("User interruption! Exiting now!")
+	except ValueError:
+		print("Incorrect value entered, please try again!")
