c549680927b7ec4305ba568fb333e396c3651d15
datboyblu3 <dayboyblu3@protonmail.com>
2021-12-08 18:38:16 -0800

    added parser to subnet_calc

--- subnet_calculator/subnet_calc.py
+++ subnet_calculator/subnet_calc.py
@@ -1,10 +1,24 @@
 import sys
 import random
 import os.path
+import argparse
 
+#Author: Daniel Edwards
+#Date: 08-12-2021
 #Validate each octet
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', '-f', help='Specify a file containing IP addresses')
+parser.add_argument('-i', '--ip', help='Specify IP address(es)')
+args = parser.parse_args()
+
+
 def validate_ip_octet(ip):
 
+	'''
+	Read input from command line for a file of IPs, single IP or
+	an x amount specified by the user
+	'''
 	try:
 
 		while True:
