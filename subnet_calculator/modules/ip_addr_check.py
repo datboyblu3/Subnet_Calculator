45f76b2182a0dfbc6652e811a41a0f1686138fc7
datboyblu3 <dayboyblu3@protonmail.com>
2021-12-11 04:41:23 -0800

    fixes

--- subnet_calculator/modules/ip_addr_check.py
+++ subnet_calculator/modules/ip_addr_check.py
@@ -6,15 +6,18 @@ import sys
 
 #Verify each octet in the IP Addresses
 def ip_addr_check(ip_list):
-
+	octets = ip_list.split('.')
+	#return print(len(octets))
 	#Different conditions for the types of iput
 	#1) Single or multiple IP Addresses submited
 	#2) A file of IP Addresses submitted
 
-	for ip in ip_list:
+	'''for ip in ip_list:
 		ip = ip.rstrip("\n")
 		octets = ip.split('.')
-		print(octets)
+		print(octets)'''
 
-		#[(ip) for ip in ip_list if(len(octets) == 4) and (all(0 <= int(octets[ip] <=255)))]
+	[int(ip) for ip in octets if(len(octets) == 4) and (all(0 <= int(octets[ip] <=255)))]
 
+
+ip_addr_check("192.168.12.4")
